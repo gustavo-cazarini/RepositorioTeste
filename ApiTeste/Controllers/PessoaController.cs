4037aa8f1da159b3c1ca0469964cf6d353e6e7b6
Gustavo Cazarini <gustavo.cazarini@outlook.com>
2025-09-09 23:40:30 -0300

    first commit

---
+++ ApiTeste/Controllers/PessoaController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ApiTeste.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PessoaController : ControllerBase
+    {
+        private readonly Pessoa[] Pessoas =
+        {
+            new Pessoa { Nome = "Gustavo", Sobrenome = "Cazarini", Idade = 24, DataNascimento = DateTime.Parse("2001/08/02"), LocalTrabalho = "JAB Advogados" },
+            new Pessoa { Nome = "Ivan", Sobrenome = "Marques", Idade = 30, DataNascimento = DateTime.Parse("1990/01/01"), LocalTrabalho = "JAB Advogados" },
+            new Pessoa { Nome = "Raimundo", Sobrenome = "Silva", Idade = 70, DataNascimento = DateTime.Parse("1955/01/01"), LocalTrabalho = "Google" }
+        };
+
+        [HttpGet("[action]")]
+        public IEnumerable<Pessoa> GetAll()
+        {
+            return Pessoas.ToArray();
+        }
+
+        [HttpGet("[action]")]
+        public Pessoa? GetByName(string nome)
+        {
+            return Pessoas.Where(pessoa => pessoa.Nome == nome).FirstOrDefault();
+        }
+    }
+}
---
+++ ApiTeste/Pessoa.cs
@@ -0,0 +1,11 @@
+﻿namespace ApiTeste
+{
+    public class Pessoa
+    {
+        public string? Nome { get; set; }
+        public string? Sobrenome { get; set; }
+        public string? LocalTrabalho { get; set; }
+        public int? Idade { get; set; }
+        public DateTime? DataNascimento { get; set; }
+    }
+}
