3c5f0e17ff7f4cf680249187fb13ff2453dd6a1b
Gustavo Cazarini <gustavocazarini@jarmandobatista.com.br>
2025-09-19 11:04:58 -0300

    feat: adição de dados estáticos para teste

---
+++ ApiTeste/Controllers/PastaController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+
+namespace ApiTeste.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PastaController : ControllerBase
+    {
+        [HttpGet("[action]")]
+        public IActionResult GetDadosPasta(string? pasta)
+        {
+            var caminhoArquivo = Path.Combine(AppContext.BaseDirectory, "Dados.json");
+
+            if (!System.IO.File.Exists(caminhoArquivo))
+            {
+                return NotFound("Arquivo Dados.json não encontrado.");
+            }
+
+            var json = System.IO.File.ReadAllText(caminhoArquivo);
+
+            return Content(json, "application/json");
+        }
+    }
+}
